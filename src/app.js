3947524ea6b35bb0ede0d6d3334399b580014a4c
y10b <victory.jun01@gmail.com>
2025-03-24 16:38:33 +0900

    CORS

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 const express = require("express")
 const router = require('./modules/index.module')
+const cors = require("cors");
 
 const app = express()
 const PORT = 5000
 
+app.use(cors({
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json())
 app.use(router)
 
-app.listen(PORT , () => {
+app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}...`)
 })
